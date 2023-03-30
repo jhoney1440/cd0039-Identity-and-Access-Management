fe2ae4ec263ae113225fa6b248f10884e6ec5f69
kashif ahmed <kashif.webpro@gmail.com>
2023-03-30 14:27:34 +0500

    Project Submission 01
    Project Submission 01

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'dev-bfkiu08eoualvgbo.us.auth0.com', // the auth0 domain prefix
+    audience: 'http://127.0.0.1:5000', // the audience set for the auth0 app
+    clientId: 'pifgl8DGELePD7Ew2ZwKBbO4erzXVcmD', // the client id generated for the auth0 app
+    callbackURL: 'https://127.0.0.1:3000/tabs/user-page', // the base url of the running ionic application. 
   }
 };
